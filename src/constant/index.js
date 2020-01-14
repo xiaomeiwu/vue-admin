86e63b21947750f318d58da45f11e055940f9af2
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-01-14 23:37:16 +0800

    feat: create constant

---
+++ src/constant/index.js
@@ -0,0 +1,20 @@
+export const sideMenus = [
+    {
+        label: "首页",
+        id: "1",
+        icon: "fa fa-home",
+        children: [{ label: "首页子菜单", id: '1-1' }]
+    },
+    {
+        label: "hhhhh",
+        id: "2",
+        icon: "fa fa-home",
+        children: [{ label: "2-1" }]
+    },
+    {
+        label: "首页",
+        id: "3",
+        icon: "fa fa-home",
+        children: [{ label: "3-1" }]
+    }
+]
