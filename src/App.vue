5c7bf3f5fb509ba7006ab3596252d85d6b534deb
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-01-15 14:25:31 +0800

    feat: create transition

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  <router-view id="app"></router-view>
+  <transition :name="transitionName" mode="out-in">
+    <router-view id="app"></router-view>
+  </transition>
 </template>
+<script>
+export default {
+  computed: {
+    transitionName() {
+      let { path } = this.$route;
+      return path === "/login" || path.indexOf("admin") > -1 ? "xm-slide" : "";
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+#app {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  overflow: hidden;
+}
+</style>
