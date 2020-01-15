181ad3b414d1b22f8b79c40ae8c1e2f29251ded7
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-01-15 14:33:11 +0800

    feat(XmInput): create XmInput

---
+++ src/components/XmInput/XmInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="xm-input">
+    <input :type="type" :value="value" @input="onInput" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: String,
+    label: String,
+    type: {
+      type: String,
+      default: "text"
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
+</script>
