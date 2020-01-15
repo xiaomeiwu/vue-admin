2ae660d9adc11cdec698949352c6d417cd31e633
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-01-15 14:32:50 +0800

    feat(XmSideMenu): create XmSideMenu

---
+++ src/components/XmSideMenu/XmSideMenuItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="xm-side-menu-item">{{data.label}}</div>
+</template>
+<script>
+export default {
+  props: {
+    data: Object
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import url("./XmSideMenuItem.scss");
+</style>
