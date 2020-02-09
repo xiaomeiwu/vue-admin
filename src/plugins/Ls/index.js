15235d5741650d3064c6fbbd524caf67e2dc5872
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-02-09 22:31:13 +0800

    feat: create Ls

--- src/plugins/Ls/index.js
+++ src/plugins/Ls/index.js
@@ -1,15 +1,46 @@
-let ls = 'Ls'
+function set(key, value) {
+  if (typeof key === 'string') {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } catch (error) {
+      return false
+    }
+  } else {
+    return false
+  }
+}
+function get(key) {
+  if (typeof key === 'string') {
+    try {
+      return JSON.parse(window.localStorage.getItem(key))
+    } catch (error) {
+      return window.localStorage.getItem(key)
+    }
+  } else {
+    return null
+  }
+}
+function clear() {
+  window.localStorage.clear()
+}
+
+const Ls = {
+  set,
+  get,
+  clear
+}
+
 let installed = false
 export default {
-    install(Vue) {
-        if (installed) {
-            return
-        }
-        installed = true
-        Object.defineProperty(Vue.prototype, 'Ls', {
-            get() {
-                return ls
-            }
-        })
+  install(Vue) {
+    if (installed) {
+      return
     }
+    installed = true
+    Object.defineProperty(Vue.prototype, 'Ls', {
+      get() {
+        return Ls
+      }
+    })
+  }
 }
