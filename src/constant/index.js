e896844f8d8a4223bd56e03021bf6c3090c3cc43
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-02-26 10:32:37 +0800

    feat: add path

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,32 +1,32 @@
 export const sideMenus = [
-    {
-        label: "首页",
-        id: "1",
-        icon: "fa fa-home",
-        children: [
-            { label: "子菜单1", id: '1-1' },
-            { label: "子菜单2", id: '1-2' },
-            { label: "子菜单3", id: '1-3' }
-        ]
-    },
-    {
-        label: "用户管理",
-        id: "2",
-        icon: "fa fa-user",
-        children: [
-            { label: "用户管理子菜单1", id: '1-1' },
-            { label: "用户管理子菜单2", id: '1-2' },
-            { label: "用户管理子菜单3", id: '1-3' }
-        ]
-    },
-    {
-        label: "订单管理",
-        id: "3",
-        icon: "fa fa-shopping-cart",
-        children: [
-            { label: "订单管理子菜单1", id: '1-1' },
-            { label: "订单管理子菜单2", id: '1-2' },
-            { label: "订单管理子菜单3", id: '1-3' }
-        ]
-    }
+  {
+    label: '首页',
+    id: '1',
+    icon: 'fa fa-home',
+    children: [
+      { label: '子菜单1', id: '1-1', path: '/admin/1-1' },
+      { label: '子菜单2', id: '1-2', path: '/admin/1-2' },
+      { label: '子菜单3', id: '1-3', path: '/admin/1-3' }
+    ]
+  },
+  {
+    label: '用户管理',
+    id: '2',
+    icon: 'fa fa-user',
+    children: [
+      { label: '用户管理子菜单1', id: '1-1', path: '/admin/2-1' },
+      { label: '用户管理子菜单2', id: '1-2', path: '/admin/2-2' },
+      { label: '用户管理子菜单3', id: '1-3', path: '/admin/2-3' }
+    ]
+  },
+  {
+    label: '订单管理',
+    id: '3',
+    icon: 'fa fa-shopping-cart',
+    children: [
+      { label: '订单管理子菜单1', id: '1-1', path: '/admin/3-1' },
+      { label: '订单管理子菜单2', id: '1-2', path: '/admin/3-2' },
+      { label: '订单管理子菜单3', id: '1-3', path: '/admin/3-3' }
+    ]
+  }
 ]
