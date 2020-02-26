68c853f40b2e12b12853219d523642d4626a5e1d
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-02-26 10:34:38 +0800

    feat: add params

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,22 +1,25 @@
-
 export default [
-    {
-        path: '/',
-        redirect: '/login'
-    },
-    {
-        path: '/login',
-        component: () => import('../../views/Login')
-    },
-    {
-        path: '/admin',
-        redirect: '/admin/dashboard',
-        component: () => import('../../views/Admin'),
-        children: [
-            {
-                path: 'dashboard',
-                component: () => import('../../views/AdminDashboard')
-            }
-        ]
-    }
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: () => import('../../views/Login')
+  },
+  {
+    path: '/admin',
+    redirect: '/admin/dashboard',
+    component: () => import('../../views/Admin'),
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('../../views/AdminDashboard')
+      },
+      {
+        path: ':id',
+        component: () => import('../../views/AdminMenu')
+      }
+    ]
+  }
 ]
